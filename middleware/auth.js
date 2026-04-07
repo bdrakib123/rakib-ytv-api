038b44e8eb6946a5144e7496c9b7a9a76202346d
Rakib <spyer.rakib@gmail.com>
2026-04-08 04:31:32 +0600

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-  const key = req.query.apikey;
+  const key = req.headers["x-api-key"];
 
   if (!key || key !== process.env.API_KEY) {
     return res.status(401).json({
